c389d13295cdbbd8bf277e692593065f6f7fcb8e
채수연 <sooyeonchae@gmail.com>
2022-05-17 10:34:07 +0900

    DIAL액션에 대한 예제코드
    - sms_body 이름표로 문구 첨부 가능.

--- app/src/main/java/net/soozip/androidintent_20220517/MainActivity.kt
+++ app/src/main/java/net/soozip/androidintent_20220517/MainActivity.kt
@@ -24,5 +24,39 @@ class MainActivity : AppCompatActivity() {
             startActivity(myIntent)
 
         }
+        btnCall.setOnClickListener{
+            //바로 전화연결 해야할 경우 권한 필요.
+
+            //1.어디에 전화를 걸지 번호를 저장
+            val inputPhoneNum = edtPhoneNum.text.toString()
+
+            //2. Uri로 전화 연결 정보를 세팅
+            val myUri = Uri.parse("tel:${inputPhoneNum}")
+
+            //3. Intent를 액션 종류 + Uri를 결합해서 생성.
+            val myIntent = Intent(Intent.ACTION_CALL, myUri)
+            //4. 실제 화면 이동.
+            startActivity(myIntent)
+
+        }
+
+        btnSms.setOnClickListener{
+            //바로 전화연결 해야할 경우 권한 필요.
+
+            //1.어디에 전화를 걸지 번호를 저장
+            val inputPhoneNum = edtPhoneNum.text.toString()
+
+            //2. Uri로 전화 연결 정보를 세팅
+            val myUri = Uri.parse("smsto:${inputPhoneNum}")
+
+            //3. Intent를 액션 종류 + Uri를 결합해서 생성.
+            val myIntent = Intent(Intent.ACTION_SENDTO, myUri)
+
+            //문자앱 화며넹 데이터 첨부
+            myIntent.putExtra("sms_body","미리 메시지를 적어봅니다.")
+            //4. 실제 화면 이동.
+            startActivity(myIntent)
+
+        }
     }
 }
